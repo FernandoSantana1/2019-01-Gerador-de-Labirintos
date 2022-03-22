3e6c1e6b83fef682d0e8a4f62c6b0aee121b8786
Fernando Santana <fernando.santana10000@gmail.com>
2022-03-21 21:59:21 -0300

    Commit

---
+++ Trabalho labirinto 2.0/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Trabalho_labirinto_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.BtnGerar = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Linen;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.Location = new System.Drawing.Point(12, 120);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(75, 41);
+            this.BtnIniciar.TabIndex = 1;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // BtnGerar
+            // 
+            this.BtnGerar.Location = new System.Drawing.Point(12, 67);
+            this.BtnGerar.Name = "BtnGerar";
+            this.BtnGerar.Size = new System.Drawing.Size(75, 47);
+            this.BtnGerar.TabIndex = 0;
+            this.BtnGerar.Text = "Gerar";
+            this.BtnGerar.UseVisualStyleBackColor = true;
+            this.BtnGerar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 41);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 22;
+            this.numericUpDown1.TabStop = false;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 167);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 35;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "Modo Slow";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(97, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(510, 510);
+            this.panel1.TabIndex = 36;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Tamanho:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(615, 541);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.BtnIniciar);
+            this.Controls.Add(this.BtnGerar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Labirinto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.Button BtnGerar;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Trabalho labirinto 2.0/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_labirinto_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = 3;
+        }
+        int contador2; bool resolver = false; int L_inicial;int C_inicial; bool resolve; 
+        Random random = new Random(); int tentativas; int casasPercorridas;  List<int> caminhoCerto = new List<int>();//removi o public
+        public int lin = 1; public int col = 1; public int count; public int tam; public List<int> PosPercorr = new List<int>(); public int escala;
+        public int posX; int[,] matriz; public int timer; int total; bool voltar = false; int prox; public List<int> verificados = new List<int>();
+        private void DesenharQuadrados(Color cor)
+        {
+            SolidBrush Solid = new SolidBrush(cor);
+            Graphics Draw = pictureBox1.CreateGraphics();
+            for (int coluna = 0; coluna < numericUpDown1.Value * escala; coluna += escala)
+            {
+                for (int linha = 0; linha < numericUpDown1.Value * escala; linha += escala)
+                {
+                    Draw.FillRectangle(Solid, linha, coluna, 1, linha);//colunas
+                    Draw.FillRectangle(Solid, linha, coluna, coluna, 1);//linhas
+                }
+            }
+        }
+        private void DesenharAtual(int linha, int coluna, Color cor, int tamanho, int direcao)
+        {
+            SolidBrush Solid = new SolidBrush(cor);
+            Graphics Draw = pictureBox1.CreateGraphics();
+            Draw.FillRectangle( Solid, coluna + 1, linha + 1, tamanho, tamanho);
+            if (direcao == 1) { Draw.FillRectangle(Solid, coluna + 1, linha - 2, tamanho, tamanho); }//   ↓
+            else if (direcao == 2) { Draw.FillRectangle(Solid, coluna - 2, linha + 1, tamanho, tamanho); }//   →
+            else if (direcao == 3) { Draw.FillRectangle(Solid, coluna + 1, linha + 2, tamanho, tamanho); }//   ↑
+            else if (direcao == 4) { Draw.FillRectangle(Solid, coluna + 2, linha + 1, tamanho, tamanho); }//   ← 
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            tam = Convert.ToInt32(numericUpDown1.Value);
+            matriz = new int[tam, tam];
+        }
+        private void VoltarCasa(int contador)
+        {
+
+            if (voltar == true)
+            {
+                void voltar_(int lin, int col)
+                {
+                    count--;
+                    contador--;
+                    matriz[lin, col] = 2;
+                    verificados.RemoveAt(contador);
+                    caminhoCerto.RemoveAt(contador); //-1 
+                }
+                if (contador - 1 > 0)
+                {
+                    if (verificados[contador - 1] == 1 & lin - 1 >= 0)
+                    {
+                        Console.WriteLine("• 1 ");
+                        if (matriz[lin - 1, col] == 1 || matriz[lin - 1, col] == 2)
+                        {
+                            lin--; Console.WriteLine("tentando voltar [1]"); voltar_(lin, col);
+                        }
+                    }
+                }
+                if (contador - 1 > 0)
+                {
+                    if (verificados[contador - 1] == 2 & col - 1 >= 0)
+                    {
+                        Console.WriteLine("• 2 ");
+                        if (matriz[lin, col - 1] == 1 || matriz[lin, col - 1] == 2)
+                        {
+                            col--; Console.WriteLine("tentando voltar [2]"); voltar_(lin, col);
+                        }
+                    }
+                }
+                if (contador - 1 > 0)
+                {
+                    if (verificados[contador - 1] == 3 & lin + 1 < tam)
+                    {
+                        Console.WriteLine("• 3");
+                        if (matriz[lin + 1, col] == 1 || matriz[lin + 1, col] == 2)
+                        {
+                            lin++; Console.WriteLine("tentando voltar [3]"); voltar_(lin, col);
+                        }
+                    }
+                }
+                if (contador - 1 > 0)
+                {
+                    if (verificados[contador - 1] == 4 & col + 1 < tam)
+                    {
+                        Console.WriteLine("• 4 ");
+                        if (matriz[lin, col + 1] == 1 || matriz[lin, col + 1] == 2)
+                        {
+                            col++; Console.WriteLine("tentando voltar [4]"); voltar_(lin, col);
+                        }
+                    }
+                }
+                if (contador - 1 <= 0 || contador2 == total & resolver == false)
+                {
+                    resolver = true;
+                    Console.WriteLine(@"\\\ FIM ///");
+                    timer1.Dispose();
+                    return;
+                }
+                void ZeroEncontrado()
+                {
+                    voltar = false;
+                    casasPercorridas = 0; tentativas = 0; PosPercorr.Clear();
+                    AvancarCasa();
+                }
+
+                if (lin - 1 >= 0)
+                {
+                    if (matriz[lin - 1, col] == 0)
+                    {
+                         ZeroEncontrado();
+
+                    }
+                }
+                if (col - 1 >= 0)
+                {
+                    if (matriz[lin, col - 1] == 0)
+                    {
+                        ZeroEncontrado();
+                    }
+                }
+                if (lin + 1 < tam)
+                {
+                    if (matriz[lin + 1, col] == 0)
+                    {
+                        ZeroEncontrado();
+                    }
+                }
+                if (col + 1 < tam)
+                {
+                    if (matriz[lin, col + 1] == 0)
+                    {
+                        ZeroEncontrado();
+                    }
+                }
+            }
+        }
+        private void AvancarCasa()
+        {
+            total = tam * tam;
+            if (voltar == false)
+            {
+                void ocuparCasa(int prox, int lin, int col)
+                {
+                    count++;
+                    casasPercorridas++;
+                    matriz[lin, col] = 1;
+                    verificados.Add(prox);
+                    caminhoCerto.Add(prox);                    
+                    contador2++;
+                    if (contador2 == 1) //primeira casa
+                    {
+                        DesenharAtual(lin * escala, col * escala, Color.NavajoWhite, escala - 1, 0); //com escala   
+                        DesenharEntrada(lin * escala, col * escala, Color.ForestGreen, escala / 2); //entrada
+                        L_inicial = lin; C_inicial = col;
+                    }
+                    else
+                    {
+                        DesenharAtual(lin * escala, col * escala, Color.NavajoWhite, escala - 1, prox); //com escala   
+                    }
+                }
+                prox = random.Next(1, 5);
+                if (!PosPercorr.Contains(prox))
+                {
+                    Console.WriteLine("Entrou em numeros sem repetir");
+                    PosPercorr.Add(prox);
+                    if (prox == 1 & lin + 1 < tam)
+                    {
+                        if (matriz[lin + 1, col] == 0)
+                        {
+                            lin++; ocuparCasa(prox, lin, col); //richDireita.AppendText(" ↓ ");
+                        }
+                    }
+                    if (prox == 2 & col + 1 < tam)
+                    {
+                        if (matriz[lin, col + 1] == 0)
+                        {
+                            col++; ocuparCasa(prox, lin, col); //richDireita.AppendText(" → ");
+                        }
+                    }
+                    if (prox == 3 & lin - 1 >= 0)
+                    {
+                        if (matriz[lin - 1, col] == 0)
+                        {
+                            lin--; ocuparCasa(prox, lin, col); //richDireita.AppendText(" ↑ ");
+                        }
+                    }
+                    if (prox == 4 & col - 1 >= 0)
+                    {
+                        if (matriz[lin, col - 1] == 0)
+                        {
+                            col--; ocuparCasa(prox, lin, col); //richDireita.AppendText(" ← ");
+                        }
+                    }
+                    
+                    tentativas++;
+                }
+                if (tentativas == 4 & casasPercorridas == 0) //se tentar as 4 possiveis direções e nao conseguir andar 1 casa, está travado.
+                {
+                    timer = 0;
+                    voltar = true;
+                    Console.WriteLine("Travado! : " + casasPercorridas);
+                    casasPercorridas = 0;
+                    tentativas = 0;
+                }
+                if (casasPercorridas != 0)
+                {
+                    Console.WriteLine("Celula encontrada... " + Environment.NewLine);
+                    casasPercorridas = 0;
+                    tentativas = 0;
+                    PosPercorr.Clear();
+                }
+            }
+        }
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            if (tam <= 200)
+            {
+                escala = pictureBox1.Width / tam;
+                pictureBox1.Height = pictureBox1.Width = Convert.ToInt32(numericUpDown1.Value * escala);
+                DesenharQuadrados(Color.DarkGoldenrod);
+            }
+            AvancarCasa(); timer1.Start();
+        }
+        public int teste2 = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (resolve == false)
+            {
+                label1.Text = "Total: " + total + " Percorridas: " + contador2;
+                total = tam * tam;
+                if (checkBox1.Checked)
+                {
+                    timer1.Interval = 200;
+                }
+                else
+                {
+                    timer1.Interval = 1;
+                }
+                timer++;
+                if (voltar == false)
+                {
+                    AvancarCasa();
+                }
+                else
+                {
+                    VoltarCasa(count);
+                }
+                if(resolver == true) //fim
+                {
+                    DesenharSaida(0, 0, Color.Purple, escala / 2);
+                    matriz[L_inicial, C_inicial] = 3; 
+                }
+            } 
+        }
+        bool pause = false;
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            if (pause == false)
+            {
+                timer1.Stop();
+            }
+            if (pause == true)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matriz.Length; i++)
+            {
+                for (int j = 0; j < matriz.Length; j++)
+                {
+                    matriz[i, j] = 0;
+                }
+            }
+            tentativas = 0; casasPercorridas = 0;  contador2 = 0;
+            lin = 1; col = 1; count = 0; tam = 0; ; PosPercorr.Clear(); escala = 0;
+            posX = 0; ; timer = 0; total = 0; voltar = false; prox = 0; ; verificados.Clear();
+        }
+        private void DesenharEntrada(int linha, int coluna, Color cor, int tamanho)
+        {
+            SolidBrush Solid = new SolidBrush(cor);
+            Graphics Draw = pictureBox1.CreateGraphics();
+            Draw.FillEllipse(Solid, coluna + 1, linha + 1, tamanho, tamanho);
+        }
+        private void DesenharSaida(int linha, int coluna, Color cor, int tamanho)
+        {
+            void desenhar(int lin, int col)
+            {
+                SolidBrush Solid = new SolidBrush(cor);
+                Graphics Draw = pictureBox1.CreateGraphics();
+                matriz[lin, col] = 4; resolver = false;
+                Draw.FillEllipse(Solid, col * escala, lin * escala, tamanho, tamanho);
+            }
+            if (resolver == true)
+            {
+                prox = random.Next(1, 3);
+                if (prox == 1) //gerar saida na direita
+                {
+                    prox = random.Next(tam / 2, tam);
+                    desenhar(tam - 1, prox);
+                }
+                if (prox == 2) //gerar saida embaixo
+                {
+                    prox = random.Next(0, tam);
+                    desenhar(prox, tam - 1);
+                }
+            }
+        }
+        private void BtnResolver_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            resolve = true;
+        }
+    }
+}
